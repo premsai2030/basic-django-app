3a68f510978d325920b28e05ab32d664473c9bde
prem sai vittal <premsai2030@gmail.com>
2022-01-16 12:31:45 +0530

    resolved conflits

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import indexPage, prem
+urlpatterns = [
+    # path("/", indexPage, name="starting page"),
+    path("prem/",prem, name="prem")
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def indexPage(request):
+    return HttpResponse("i am starting page")
+
+def prem(request):
+    return HttpResponse("hello mate this is prem sai vittal")
